a1c49bc666cd39154ccb9dd7b5a9588cf85c7b91
nathalyzavala <24iveth.ramirez@gmail.com>
2021-06-16 22:34:12 -0300

    create connection

--- config/Connection.php
+++ config/Connection.php
@@ -1,7 +1,7 @@
 <?php 
 	// $conn = new mysqli("localhost","root","","user");
 
-   // if($conn->connect_errno){
+    if($conn->connect_errno){
    //     die("Connection Failed".$conn->connect_errno);
-   // }
+   }
  ?>
